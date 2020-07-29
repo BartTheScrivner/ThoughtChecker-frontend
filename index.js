abe8e15a2e075b571391b4a25ffcad637e06ecc1
BartTheScrivner <dframe22@gmail.com>
2020-07-29 16:03:02 -0400

    done with posting entries

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const banner = document.getElementById('banner');
-const entryForm = document.getElementById('entry-form');
+const formField = document.getElementById('form-field');
+const dashboard = document.getElementById('dashboard');
+const calendar = document.getElementById('calendar')
+const friendMenu = document.getElementById('friend-menu')
 
 document.addEventListener("DOMContentLoaded", () => {
   promptUserInfo()
 });
 
+// LOGIN
 function promptUserInfo() {
   const welcome = document.createElement('h1');
   const userForm = document.createElement('form');
@@ -17,8 +21,8 @@ function promptUserInfo() {
   userForm.appendChild(subBtn)
   welcome.innerText = "Hello, what's your name?"
   banner.appendChild(welcome)
-  entryForm.appendChild(userForm)
-  entryForm.addEventListener('submit', e => {
+  formField.appendChild(userForm)
+  formField.addEventListener('submit', e => {
     e.preventDefault();
     sendLogIn(userForm);
   });
@@ -35,8 +39,97 @@ function sendLogIn(userForm) {
   }
   fetch('http://localhost:3000/login', postRequest)
   .then(response => response.json())
-  .then(console.log)
+  .then(user => renderDashboard(user))
 }
+
+function renderDashboard(user) {
+  const date = document.getElementById('date');
+  date.innerText = moment().format('ll');
+  banner.innerHTML = ``;
+  formField.innerHTML = ``;
+  const welcomeMessage = document.createElement("h2");
+  const dailyQuote = document.createElement('h3');
+  welcomeMessage.innerText = `Welcome, ${user.name}.`;
+  dailyQuote.innerText = fetchQuote();
+  banner.appendChild(welcomeMessage);
+  renderCalendar(user)
+  renderFriendMenu(user)
+  toggleModal();
+  setUser(user);
+}
+
+function setUser(user) {
+  const entryForm = document.getElementById('entry-form')
+  entryForm.dataset.userId = user.id;
+}
+
+// DASHBOARD
+function renderCalendar(user) {
+  // const calendar = new Calendar('#calendar', user.entries);
+}
+
+function renderFriendMenu(user) {
+  user.friendships.forEach(friend => {
+    renderFriend(friend)
+  })
+}
+
+function renderFriend(friend) {
+  const friendCard = document.createElement('li');
+  friendCard.innerText = friend.name;
+  friendMenu.appendChild(friendCard);
+}
+
+function fetchQuote() {
+  return "Don't judge each day by the harvest you reap but by the seeds that you plant. -Robert Louis Stevenson"
+}
+
+// MODAL ENTRY FORM
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close-button");
+const newEntry = document.getElementById('new-entry')
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+closeButton.addEventListener("click", () =>{
+  toggleModal()
+});
+
+newEntry.addEventListener("click", () =>{
+  toggleModal()
+});
+
+const entryForm = document.getElementById('entry-form')
+entryForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  postEntry(entryForm);
+});
+
+function postEntry(entryForm) {
+  const formFields = {
+    user_id: entryForm.dataset.userId,
+    mood: entryForm.mood.value,
+    narrative: entryForm.narrative.value.trim()
+  }
+
+  const postRequest = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(formFields)
+  }
+
+  fetch('http://localhost:3000/entries', postRequest)
+  .then(response => response.json())
+  .then(console.log);
+
+  toggleModal();
+}
+
 
 
 
